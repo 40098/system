da3e1384ea3b22cf6a5310290e2b0c449123a453
40098 <40098@hoornbeeck.nl>
2021-03-10 07:55:14 +0100

    name customer.name required in db

--- database/migrations/2020_10_30_201632_create_customers_table.php
+++ database/migrations/2020_10_30_201632_create_customers_table.php
@@ -15,7 +15,7 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
+            $table->string('name');
             $table->string('mobile_phone')->nullable();
             $table->string('house_phone')->nullable();
             $table->string('email')->nullable();
